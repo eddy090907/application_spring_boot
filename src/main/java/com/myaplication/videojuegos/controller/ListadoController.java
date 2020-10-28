64cbc42bdda645c62b89c22cb240e86096b30ba7
Edward Gomez Orjuela <1558453@TCS.com>
2020-10-28 18:19:10 -0500

    First Commit

---
+++ src/main/java/com/myaplication/videojuegos/controller/ListadoController.java
@@ -0,0 +1,17 @@
+package com.myaplication.videojuegos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ListadoController {
+
+    @RequestMapping(value="/")
+    public String listarVideoJuegos() {
+        return "listado";
+    }
+
+}
